3e04f862a1fef8465c11c2d1983f914cc8faafb7
Markimg22 <marquim.net.221200@hotmail.com>
2025-03-06 21:32:24 -0400

    aula-3

---
+++ aula-3/src/Main.java
@@ -0,0 +1,85 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // Estruturas de Decisão
+        // if, else, else if, switch...
+
+//
+//        boolean condicao = false;
+//
+//        if (condicao) {
+//            // se a condição for verdade
+//            System.out.println("Condição verdadeira");
+//        } else {
+//            // se a condição for falsa
+//            System.out.println("Condição falsa");
+//        }
+//
+//        Scanner scanner = new Scanner(System.in);
+//
+//        System.out.println("Digite sua idade:");
+//        int idade = scanner.nextInt();
+//
+//        if (idade >= 18) {
+//            System.out.println("Maior de idade.");
+//        } else {
+//            System.out.println("Menor de idade.");
+//        }
+
+//        boolean condicao1 = true;
+//        boolean condicao2 = true;
+//
+//        if (condicao1) {
+//            System.out.println("Condicao 1");
+//        } else if (condicao2) {
+//            System.out.println("Condicao 2");
+//        } else {
+//            System.out.println("Nenhuma condicao é verdade");
+//        }
+
+        // switch
+//        int valor = 5;
+//
+//        switch (valor) {
+//            case 1:
+//                System.out.println("Valor é 1");
+//                break;
+//            case 2:
+//                System.out.println("Valor é 2");
+//                break;
+//            case 3: case 4:
+//                System.out.println("Valor é 3 ou 4");
+//                break;
+//            default:
+//                System.out.println("Não caiu em nenhum caso.");
+//        }
+
+        // if ternário
+//        int idade = 18;
+//        String resultado = idade >= 18 ? "Maior de idade" : "Menor de idade";
+//        System.out.println(resultado);
+
+
+        // Estrutura de Repetição (loops)
+        // for, while, do while
+
+//        for (int i = 0; i <= 10; i++) {
+//            System.out.println(i);
+//        }
+
+        // while - enquanto
+//        int count = 0;
+//        while (count <= 10) {
+//            System.out.println("Teste");
+//            count += 1;
+//
+//            if (count == 5) break;
+//        }
+
+        // do while
+//        do {
+//            System.out.println("Caiu no do while");
+//        } while (false);
+    }
+}
