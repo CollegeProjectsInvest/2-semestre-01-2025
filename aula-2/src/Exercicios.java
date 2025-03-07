b809d9b765e767527310805340d16f5c6d85496c
Markimg22 <marquim.net.221200@hotmail.com>
2025-03-06 20:58:01 -0400

    aula-2 - exercicios

---
+++ aula-2/src/Exercicios.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+
+public class Exercicios {
+    public static void main(String[] args) {
+        // 1
+//        String nome = "Marcos Campos";
+//        String email = "marcos@hotmail.com";
+//        System.out.println("Olá " + nome + "! Seu e-mail " + email + " foi cadastrado.");
+
+        // 2
+        // objeto da classe Scanner
+//        Scanner scanner = new Scanner(System.in);
+//
+//        System.out.println("Digite seu nome: ");
+//        String nome = scanner.nextLine();
+//
+//        System.out.println("Digite seu e-mail: ");
+//        String email = scanner.nextLine();
+//
+//        System.out.println("Olá " + nome + "! Seu e-mail " + email + " foi cadastrado.");
+
+        // 3
+//        Scanner scanner = new Scanner(System.in);
+//
+//        System.out.println("Digite seu peso: ");
+//        double peso = scanner.nextDouble();
+//
+//        System.out.println("Digite sua altura: ");
+//        double altura = scanner.nextDouble();
+//
+//        double imc = peso / (altura * altura);
+//
+//        System.out.println("Seu IMC: " + Math.floor(imc));
+    }
+}
