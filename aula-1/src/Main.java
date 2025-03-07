7a986b9a65360673a8ea8d2f83505be1225d0233
Markimg22 <marquim.net.221200@hotmail.com>
2025-03-06 20:05:41 -0400

    aula-2

--- aula-1/src/Main.java
+++ aula-1/src/Main.java
@@ -1,15 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        int idade = 18;
-        boolean maiorDeIdade = true; // false
-        float altura = 1.80F;
-        double salario = 1000.50;
-        String nome = "Marcos";
-        char sexo = 'M';
 
-        int soma = 10 + 12;
-
-        boolean verdade = 10 != 10;
-        System.out.println(verdade);
     }
 }
---
+++ aula-2/src/Main.java
@@ -0,0 +1,27 @@
+public class Main {
+    public static void main(String[] args) {
+        // Tipos Primitivos
+        // tipo nomeVariavel;
+        // tipo nomeVarial = valor;
+
+        int idade = 10;
+        char caractere = 'A';
+        boolean valor = true;
+        double altura = 1.80;
+        float peso = 19.0f;
+        String nome = "Marcos"; // String é uma classe
+
+        // Operadores
+        int soma = 10 + 1;
+        int multi = 9 * 2;
+
+        System.out.println(soma);
+        System.out.println(multi);
+
+        boolean valor = true && false; // AND
+        boolean valorOr = false || false; // OR
+        boolean valorNot = !!true; // NOT
+
+        System.out.println(valorNot);
+    }
+}
