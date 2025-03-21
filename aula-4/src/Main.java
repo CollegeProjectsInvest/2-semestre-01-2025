d6adbe99b43f2ab57838de8827d034c85fa78c2b
Markimg22 <marquim.net.221200@hotmail.com>
2025-03-20 21:14:49 -0400

    aula-4

--- aula-4/src/Main.java
+++ aula-4/src/Main.java
@@ -1,37 +1,59 @@
-import java.util.Random;
-import java.util.Scanner;
+import java.util.ArrayList;
+import java.util.HashMap;
 
 public class Main {
     public static void main(String[] args) {
-        // 7
-        Random random = new Random();
-        int numeroAleatorio = random.nextInt(101);
+        // array
+//        int[] numeros = new int[4];
+//        numeros[0] = 10;
+//        numeros[1] = 5;
+//        numeros[2] = 11;
+//
+//        String[] nomes = {
+//                "MArcos",
+//                "JoÃo",
+//                "Tiago",
+//                "luis"
+//        };
+//        System.out.println("Nome 1: " + nomes[0]);
+//
+//        for (int i = 0; i < 4; i++) {
+//            System.out.println(numeros[i]);
+//        }
 
-        int tentativas = 0;
+        // ArrayList
+//        ArrayList<String> nomes = new ArrayList<>();
+//        nomes.add("Marcos");
+//        nomes.add("Teste 11");
+//        nomes.add("Teste 2");
+//
+//        for (String nome : nomes) {
+//            System.out.println(nome);
+//        }
+//
+//        nomes.remove(0);
+//
+//        System.out.println(nomes);
+//
+//        System.out.println(nomes.get(0));
+//        System.out.println("Tamanho: " + nomes.size());
+//
+//        System.out.println(nomes.contains("Teste 3"));
+//
+//        nomes.clear();
+//        System.out.println(nomes.isEmpty());
 
-        Scanner scanner = new Scanner(System.in);
+        // HashMap
+        HashMap<String, String> agenda = new HashMap<>();
+        agenda.put("651918131", "Marcos");
+        agenda.put("651918130", "João");
 
-        while (true) {
-            // capturando palpite
-            System.out.println("Digite o palpite entre 0 à 100:");
-            int palpite = scanner.nextInt();
+        System.out.println(agenda);
 
-            tentativas++;
-
-            // Comparações
-            if (tentativas == 5) {
-                System.out.println("Perdeu! Seu número de tentativas acabou!");
-                break;
-            }
-
-            if (palpite > numeroAleatorio) {
-                System.out.println("O número aleatório é menor.");
-            } else if (palpite < numeroAleatorio) {
-                System.out.println("O número aleatório é maior");
-            } else {
-                System.out.println("Parabéns você acertou: " + numeroAleatorio);
-                break;
-            }
-        }
+        System.out.println(agenda.get("651918130"));
+        System.out.println(agenda.containsValue("Marcos"));
+        System.out.println(agenda.size());
+        agenda.remove("651918131");
+        System.out.println(agenda);
     }
 }
