8dd2298616d037faee1bf3f046586e01da89d993
Markimg22 <marquim.net.221200@hotmail.com>
2025-05-15 20:11:56 -0400

    feat: aula 8

---
+++ aula-8/src/Main.java
@@ -0,0 +1,32 @@
+class Cliente {
+    protected String nome;
+    protected int idade;
+
+    public Cliente(String nome, int idade) {
+        this.nome = nome;
+        this.idade = idade;
+    }
+
+    protected void cadastrar() {
+        System.out.println("Nome Cliente: " + this.nome);
+    }
+}
+
+class ClienteVip extends Cliente {
+    public ClienteVip(String nome, int idade) {
+        super(nome, idade);
+    }
+
+    @Override
+    protected void cadastrar() {
+        super.cadastrar();
+        System.out.println("Nome Cliente Vip: " + this.nome);
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        ClienteVip clienteVip = new ClienteVip("Marcos", 21);
+        clienteVip.cadastrar();
+    }
+}
